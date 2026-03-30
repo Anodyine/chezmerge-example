7a17c98fe1715d29ef508eb4a3389eb5cddb6cfe
Kyle Merritt <ky562730@ucf.edu>
2026-03-30 15:49:29 -0400

    updated workspace setup

--- dot_config/hypr/scripts/executable_loadconfig.sh
+++ dot_config/hypr/scripts/executable_loadconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 hyprctl reload
 
-if [ "$(uname -n)" = "arch-surface" ]; then
+if [ "$(uname -n)" = "arch-surface" ] || [ "$(uname -n)" = "arch-lenovo" ]; then
     ~/.config/hypr/scripts/assign-workspaces.sh
 fi
